c9f0b70279a89a142da905c2d987d140bf968122
Roshan <luoshen1997@gmail.com>
2022-12-12 14:58:39 +0800

    fix default keyring algo type

--- client/keys/add.go
+++ client/keys/add.go
@@ -8,6 +8,7 @@ import (
 	"sort"
 
 	"github.com/cosmos/go-bip39"
+	ethHd "github.com/evmos/ethermint/crypto/hd"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -76,7 +77,7 @@ Example:
 	f.Uint32(flagCoinType, sdk.GetConfig().GetCoinType(), "coin type number for HD derivation")
 	f.Uint32(flagAccount, 0, "Account number for HD derivation (less than equal 2147483647)")
 	f.Uint32(flagIndex, 0, "Address index number for HD derivation (less than equal 2147483647)")
-	f.String(flags.FlagKeyAlgorithm, string(hd.Secp256k1Type), "Key signing algorithm to generate keys for")
+	f.String(flags.FlagKeyAlgorithm, string(ethHd.EthSecp256k1Type), "Key signing algorithm to generate keys for")
 
 	return cmd
 }
