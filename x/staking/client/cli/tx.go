e000318fae478115596ff0f291c34a23ab84f64d
Keefe Liu <keefe.l@binance.com>
2022-12-16 14:45:37 +0800

    delete useless codes

--- x/staking/client/cli/tx.go
+++ x/staking/client/cli/tx.go
@@ -41,55 +41,14 @@ func NewTxCmd() *cobra.Command {
 	}
 
 	stakingTxCmd.AddCommand(
-		NewCreateValidatorCmd(),
 		NewEditValidatorCmd(),
 		NewDelegateCmd(),
-		NewRedelegateCmd(),
+		//NewRedelegateCmd(), // Redelegate is not supported now.
 		NewUnbondCmd(),
 		NewCancelUnbondingDelegation(),
 	)
 
 	return stakingTxCmd
-}
-
-// NewCreateValidatorCmd returns a CLI command handler for creating a MsgCreateValidator transaction.
-func NewCreateValidatorCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "create-validator",
-		Short: "create new validator initialized with a self-delegation to it",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-
-			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags()).
-				WithTxConfig(clientCtx.TxConfig).WithAccountRetriever(clientCtx.AccountRetriever)
-			txf, msg, err := newBuildCreateValidatorMsg(clientCtx, txf, cmd.Flags())
-			if err != nil {
-				return err
-			}
-
-			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
-		},
-	}
-
-	cmd.Flags().AddFlagSet(FlagSetPublicKey())
-	cmd.Flags().AddFlagSet(FlagSetAmount())
-	cmd.Flags().AddFlagSet(flagSetDescriptionCreate())
-	cmd.Flags().AddFlagSet(FlagSetCommissionCreate())
-	cmd.Flags().AddFlagSet(FlagSetMinSelfDelegation())
-
-	cmd.Flags().String(FlagIP, "", fmt.Sprintf("The node's public IP. It takes effect only when used in combination with --%s", flags.FlagGenerateOnly))
-	cmd.Flags().String(FlagNodeID, "", "The node's ID")
-	flags.AddTxFlagsToCmd(cmd)
-
-	_ = cmd.MarkFlagRequired(flags.FlagFrom)
-	_ = cmd.MarkFlagRequired(FlagAmount)
-	_ = cmd.MarkFlagRequired(FlagPubKey)
-	_ = cmd.MarkFlagRequired(FlagMoniker)
-
-	return cmd
 }
 
 // NewEditValidatorCmd returns a CLI command handler for creating a MsgEditValidator transaction.
@@ -349,81 +308,6 @@ $ %s tx staking cancel-unbond 0x91D7d.. 100stake 2 --from mykey
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
-}
-
-func newBuildCreateValidatorMsg(clientCtx client.Context, txf tx.Factory, fs *flag.FlagSet) (tx.Factory, *types.MsgCreateValidator, error) {
-	return txf, nil, nil
-	// TODO: delete this cmd totally later.
-	//fAmount, _ := fs.GetString(FlagAmount)
-	//amount, err := sdk.ParseCoinNormalized(fAmount)
-	//if err != nil {
-	//	return txf, nil, err
-	//}
-	//
-	//valAddr := clientCtx.GetFromAddress()
-	//pkStr, err := fs.GetString(FlagPubKey)
-	//if err != nil {
-	//	return txf, nil, err
-	//}
-	//
-	//var pk cryptotypes.PubKey
-	//if err := clientCtx.Codec.UnmarshalInterfaceJSON([]byte(pkStr), &pk); err != nil {
-	//	return txf, nil, err
-	//}
-	//
-	//moniker, _ := fs.GetString(FlagMoniker)
-	//identity, _ := fs.GetString(FlagIdentity)
-	//website, _ := fs.GetString(FlagWebsite)
-	//security, _ := fs.GetString(FlagSecurityContact)
-	//details, _ := fs.GetString(FlagDetails)
-	//description := types.NewDescription(
-	//	moniker,
-	//	identity,
-	//	website,
-	//	security,
-	//	details,
-	//)
-	//
-	//// get the initial validator commission parameters
-	//rateStr, _ := fs.GetString(FlagCommissionRate)
-	//maxRateStr, _ := fs.GetString(FlagCommissionMaxRate)
-	//maxChangeRateStr, _ := fs.GetString(FlagCommissionMaxChangeRate)
-	//
-	//commissionRates, err := buildCommissionRates(rateStr, maxRateStr, maxChangeRateStr)
-	//if err != nil {
-	//	return txf, nil, err
-	//}
-	//
-	//// get the initial validator min self delegation
-	//msbStr, _ := fs.GetString(FlagMinSelfDelegation)
-	//
-	//minSelfDelegation, ok := sdk.NewIntFromString(msbStr)
-	//if !ok {
-	//	return txf, nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "minimum self delegation must be a positive integer")
-	//}
-	//
-	//msg, err := types.NewMsgCreateValidator(
-	//	sdk.ValAddress(valAddr), pk, amount, description, commissionRates, minSelfDelegation,
-	//)
-	//if err != nil {
-	//	return txf, nil, err
-	//}
-	//var msg *types.MsgCreateValidator
-	//if err := msg.ValidateBasic(); err != nil {
-	//	return txf, nil, err
-	//}
-	//
-	//genOnly, _ := fs.GetBool(flags.FlagGenerateOnly)
-	//if genOnly {
-	//	ip, _ := fs.GetString(FlagIP)
-	//	nodeID, _ := fs.GetString(FlagNodeID)
-	//
-	//	if nodeID != "" && ip != "" {
-	//		txf = txf.WithMemo(fmt.Sprintf("%s@%s:26656", nodeID, ip))
-	//	}
-	//}
-	//
-	//return txf, msg, nil
 }
 
 // Return the flagset, particular flags, and a description of defaults
