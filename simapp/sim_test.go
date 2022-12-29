dfbee4c2ef493a75ad4f7906a07295b38c4d00bc
KeefeL <90749943+KeefeL@users.noreply.github.com>
2022-12-29 14:29:20 +0800

    disable create validator operation (#79)

--- simapp/sim_test.go
+++ simapp/sim_test.go
@@ -286,8 +286,9 @@ func TestAppSimulationAfterImport(t *testing.T) {
 // TODO: Make another test for the fuzzer itself, which just has noOp txs
 // and doesn't depend on the application.
 func TestAppStateDeterminism(t *testing.T) {
-	// TODO skip first, Keefe fix it later
-	t.Skip("skipping application simulation")
+	if !FlagEnabledValue {
+		t.Skip("skipping application simulation")
+	}
 
 	config := NewConfigFromFlags()
 	config.InitialBlockHeight = 1
