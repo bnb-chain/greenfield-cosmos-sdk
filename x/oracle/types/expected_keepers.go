dd9cbde4e79bf6f5307f918d8ba3d67ba2eefd44
Ethan <cosinlinker@gmail.com>
2023-03-09 11:13:15 +0800

    feat(cross-chain): add callbackGasPrice to cross-chain package (#121)

--- x/oracle/types/expected_keepers.go
+++ x/oracle/types/expected_keepers.go
@@ -16,6 +16,7 @@ type StakingKeeper interface {
 type CrossChainKeeper interface {
 	CreateRawIBCPackageWithFee(ctx sdk.Context, channelID sdk.ChannelID,
 		packageType sdk.CrossChainPackageType, packageLoad []byte, relayerFee *big.Int, ackRelayerFee *big.Int,
+		callbackGasPrice *big.Int,
 	) (uint64, error)
 	GetCrossChainApp(channelID sdk.ChannelID) sdk.CrossChainApplication
 	GetSrcChainID() sdk.ChainID
