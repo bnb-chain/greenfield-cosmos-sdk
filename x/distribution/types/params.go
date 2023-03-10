21c949bb25a4f8b696cc50273d03b626be4fea5c
Keefe Liu <keefe.l@binance.com>
2023-03-10 09:53:06 +0800

    revert change community tax

--- x/distribution/types/params.go
+++ x/distribution/types/params.go
@@ -25,7 +25,7 @@ func ParamKeyTable() paramtypes.KeyTable {
 // DefaultParams returns default distribution parameters
 func DefaultParams() Params {
 	return Params{
-		CommunityTax:        sdk.ZeroDec(),            // 0%
+		CommunityTax:        sdk.NewDecWithPrec(2, 2), // 2%
 		BaseProposerReward:  sdk.NewDecWithPrec(1, 2), // 1%
 		BonusProposerReward: sdk.NewDecWithPrec(4, 2), // 4%
 		WithdrawAddrEnabled: true,
