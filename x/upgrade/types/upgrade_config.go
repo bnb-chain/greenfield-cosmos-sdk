1a504101bd5e155e25c202d057ddbd0acb6b9a3f
dylanhuang <j75689@gmail.com>
2023-08-04 14:55:18 +0800

    feat: add Nagqu fork name (#256)
    * feat: add EnableGroupMemberExpirationUpgrade
    
    * fix: update upgrade name
    
    * fix: update hardfork name

--- x/upgrade/types/upgrade_config.go
+++ x/upgrade/types/upgrade_config.go
@@ -7,6 +7,10 @@ import (
 const (
 	// EnablePublicDelegationUpgrade is the upgrade name for enabling public delegation
 	EnablePublicDelegationUpgrade = "EnablePublicDelegationUpgrade"
+
+	// Nagqu is the upgrade name for following features:
+	// - group member expiration
+	Nagqu = "Nagqu"
 )
 
 // The default upgrade config for networks
