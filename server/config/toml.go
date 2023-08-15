833c0d4721b48df7c5aa5c5b72b68a722314ad9d
forcodedancing <just.haha.it@gmail.com>
2023-08-15 15:13:43 -0600

    fix issues

--- server/config/toml.go
+++ server/config/toml.go
@@ -97,6 +97,7 @@ enable-unsafe-query = "{{ .BaseConfig.EnableUnsafeQuery }}"
 
 # EnablePlainStore enables or disables the plain store. If it is true, then plain store will be used, not IAVL store.
 # Do not enable it on validator nodes or other nodes which require high security level.
+# If you enable it, please also enable skip_app_hash config in config.toml file.
 # Default is false.
 enable-plain-store = "{{ .BaseConfig.EnablePlainStore }}"
 
